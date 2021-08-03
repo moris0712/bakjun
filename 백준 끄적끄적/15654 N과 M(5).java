ecb8332899d5cb0040d4aa6d906fab9bad9869f1
박준영 <ib0712@naver.com>
2021-08-03 11:09:33 +0900

    Add files via upload

---
+++ 백준 끄적끄적/15654 N과 M(5).java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class ccc {
+
+    static int arr [];
+    static int N;
+    static int M;
+    static int print_arr[];
+    static boolean visited[];
+
+    public static void sequence(int my_pos, int depth){
+
+        if(depth == M){
+            for(int val : print_arr)
+                System.out.print(val+" ");
+            System.out.println();
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            print_arr[depth] = arr[i];
+
+            if(!visited[i]){
+                visited[i] = true;
+                sequence(i, depth + 1);
+                visited[i] = false;
+            }
+                
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        visited = new boolean[N];
+        print_arr = new int[M];
+
+
+        for(int i=0; i<N; i++)
+            arr[i] = sc.nextInt();
+            
+        Arrays.sort(arr);
+
+        sequence(0, 0);
+
+
+        
+    }
+}
+
+
