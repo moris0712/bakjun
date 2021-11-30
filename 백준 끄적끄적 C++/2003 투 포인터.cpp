e711a880d1d0b051a322e45bc8c3b143334a2348
박준영 <ib0712@naver.com>
2021-11-30 22:22:27 +0900

    Add files via upload

--- 백준 끄적끄적 C++/2003 투 포인터.cpp
+++ 백준 끄적끄적 C++/2003 투 포인터.cpp
@@ -6,44 +6,53 @@ using namespace std;
 // std 클래스
 // namespace에 있는 std 클래스에 정의되어있는 함수를 사용하겠다.
 
-int main(){
+// 투 포인터
+
+// 이중 포문 O(n^2) 대신 O(n) 사용
+int main()
+{
     int N;
     cin >> N;
-    
+
     int M;
     cin >> M;
 
     int arr[N];
-    for(int i=0; i<N; i++)
+    for (int i = 0; i < N; i++)
         cin >> arr[i];
 
     int count = 0;
 
-    int left=0;
-    int right=0;
-    int sum=arr[0];
+    int left = 0;
+    int right = 0;
+    int sum = arr[0];
 
-    while(left <= right && right < N){
+    while (left <= right && right < N)
+    {
 
-        if(sum < M){
+        if (sum < M)
+        {
             right++;
             if (right >= N) // c에서 int배열은 EOF를 찾을수없다
                 break;
             sum += arr[right];
         }
-        else if(sum == M){
+        else if (sum == M)
+        {
             count++;
             right++;
             if (right >= N) // c에서 int배열은 EOF를 찾을수없다
                 break;
             sum += arr[right];
         }
-        else if(sum > M){
+        else if (sum > M)
+        {
             sum -= arr[left];
             left++;
             if (left >= N) // c에서 int배열은 EOF를 찾을수없다
                 break;
-            if (left > right && left < N){ // left가 right역전 하였을때
+            if (left > right && left < N)
+            { // left가 right역전 하였을때
                 right = left;
                 sum = arr[right];
             }
